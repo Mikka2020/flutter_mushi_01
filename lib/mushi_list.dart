8d55060fbc052b368b4bef29da2effbd27fb6508
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-07-15 14:23:56 +0900

    [create]新規作成

---
+++ lib/mushi_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MushiList extends StatelessWidget {
+  const MushiList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: (const Text('Mushi List')),
+    );
+  }
+}
---
+++ lib/settings.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: (const Text('Settings')),
+    );
+  }
+}

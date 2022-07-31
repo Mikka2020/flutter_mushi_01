a4b3d4af1500c4fc3307a902a3fcfb1a2f117f13
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-07-31 11:40:18 +0900

    [add]ホーム画面の仮組み作成

---
+++ lib/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Spacer(),
+          Container(
+            alignment: Alignment.center,
+            width: 200,
+            height: 100,
+            color: Colors.amber,
+            child: const Text("見つけた数"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () => {},
+                child: const Text("あそびかた"),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                child: const Text("（仮）"),
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () => {},
+            child: const Text("ずかんを見る"),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}

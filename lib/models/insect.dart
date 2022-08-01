be694fdaea336819cc3245403db5fe2e01896991
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-08-01 22:19:49 +0900

    [create]insectクラス作成

---
+++ lib/models/insect.dart
@@ -0,0 +1,13 @@
+class Insect {
+  final int id;
+  final String name;
+  final String description;
+  final String imagePath;
+
+  Insect({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.imagePath,
+  });
+}

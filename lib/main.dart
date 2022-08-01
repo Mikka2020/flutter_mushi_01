c25de164fcd0c1978391be695310e6df2106c55d
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-08-01 22:19:28 +0900

    [add]撮影機能を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import './home.dart';
+import './image_select.dart';
 import './map.dart';
-import './mushi_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,33 +19,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mushi Collection',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const TopPage(title: 'むしこれ'),
+      home: const TopPage(),
     );
   }
 }
 
 class TopPage extends StatefulWidget {
-  const TopPage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const TopPage({Key? key}) : super(key: key);
   @override
   State<TopPage> createState() => _TopPageState();
 }
 
 class _TopPageState extends State<TopPage> {
   int currentBtmNav = 0;
+  // カメラ機能の処理
+  File? image;
+  late var decodeResult;
+  final picker = ImagePicker();
+  Future getImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        image = File(pickedFile.path);
+        debugPrint("setデバッグ：" + image.toString());
+      }
+    });
+    if (image != null) {
+      debugPrint("pushデバッグ: " + image.toString());
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (BuildContext context) => ImageSelect(image: image!),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: IndexedStack(
-          children: const [Home(), MushiList(), Map()],
+          children: const [
+            Home(),
+            Map(),
+          ],
           index: currentBtmNav,
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        child: const Icon(Icons.add_a_photo),
       ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) => {
@@ -54,10 +87,6 @@ class _TopPageState extends State<TopPage> {
           BottomNavigationBarItem(
             icon: Icon(Icons.list),
             label: 'ホーム',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu_book),
-            label: 'さつえい',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),

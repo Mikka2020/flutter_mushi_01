85e57a9e25030ee10b49bfd7df1255758be50413
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-08-01 23:06:36 +0900

    [update]btmnavbarを切り別ファイルに分割

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
+import './floor_map.dart';
 import './home.dart';
-import './image_select.dart';
-import './map.dart';
+import './indexed_stack_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,81 +15,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const TopPage(),
-    );
-  }
-}
-
-class TopPage extends StatefulWidget {
-  const TopPage({Key? key}) : super(key: key);
-  @override
-  State<TopPage> createState() => _TopPageState();
-}
-
-class _TopPageState extends State<TopPage> {
-  int currentBtmNav = 0;
-  // カメラ機能の処理
-  File? image;
-  late var decodeResult;
-  final picker = ImagePicker();
-  Future getImage() async {
-    final pickedFile = await picker.pickImage(source: ImageSource.camera);
-
-    setState(() {
-      if (pickedFile != null) {
-        image = File(pickedFile.path);
-        debugPrint("setデバッグ：" + image.toString());
-      }
-    });
-    if (image != null) {
-      debugPrint("pushデバッグ: " + image.toString());
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (BuildContext context) => ImageSelect(image: image!),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: IndexedStack(
-          children: const [
-            Home(),
-            Map(),
-          ],
-          index: currentBtmNav,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        child: const Icon(Icons.add_a_photo),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) => {
-          setState(
-            () => currentBtmNav = index,
-          ),
-        },
-        currentIndex: currentBtmNav,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'ホーム',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'マップ',
-          ),
-        ],
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        initialRoute: IndexedStackBar.btmNavId,
+        routes: {
+          Home.btmNavId: (context) => const Home(),
+          FloorMap.btmNavId: (context) => const FloorMap(),
+          IndexedStackBar.btmNavId: (context) => const IndexedStackBar(),
+        });
   }
 }

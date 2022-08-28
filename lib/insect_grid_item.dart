67562fc834a15faf09e491bd4bca8ae6c90df59a
mikka2020 <70931676+Mikka2020@users.noreply.github.com>
2022-08-28 18:07:34 +0900

    むしリストからむし詳細画面への遷移を追加

--- lib/insect_grid_item.dart
+++ lib/insect_grid_item.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 
+import './insect_detail.dart';
+
 class InsectGridItem extends StatelessWidget {
   const InsectGridItem({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            color: Colors.red.withOpacity(0.5),
-            borderRadius: BorderRadius.circular(10),
-            image: const DecorationImage(
-              image: AssetImage('assets/no_image.png'),
-              fit: BoxFit.fitWidth,
+        InkWell(
+          onTap: () => {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const InsectDetail(),
+              ),
+            ),
+          },
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.green.withOpacity(.5),
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: AssetImage("assets/no_image.png"),
+                fit: BoxFit.fitWidth,
+              ),
             ),
           ),
         ),
